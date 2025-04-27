ec2da6662388b92f2a342ad15300d3e4e76d754a
Ayush <Ayushkar2005@gmail.com>
2025-04-27 15:45:00 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical-Chatbot",
+    version='0.0.0',
+    author="Ayush Kar",
+    author_email = 'ayushkar1947@gmail.com',
+    packages = find_packages(),
+    install_requires = [],
+
+)
